fbc96e9f8868f7c771581efe3b24eab669e2cdbc
Adnan Memic <adnan.memic27@gmail.com>
2026-09-24 01:21:11 +0200

    neovim: update treesitter

--- nvim/lua/adnan/plugins/treesitter.lua
+++ nvim/lua/adnan/plugins/treesitter.lua
@@ -4,11 +4,20 @@ return{
 	lazy = false,
 	build = ':TSUpdate',
 	config = function()
-		require('nvim-treesitter').install({
+		local parsers = {
 			'java',
 			'python',
 			'lua',
 			'c'
-		})
+		}
+
+		require('nvim-treesitter').install(parsers)
+
+		vim.api.nvim_create_autocmd("FileType", {
+            pattern = parsers,
+            callback = function()
+                vim.treesitter.start()
+            end,
+        })
 	end
 }
