88484ba6c1194e359474461fc64b4b9d9ba36196
Adnan Memic <adnan.memic27@gmail.com>
2026-09-25 15:19:50 +0200

    neovim: add onedark to themes

---
+++ nvim/lua/adnan/plugins/themes.lua
@@ -0,0 +1,37 @@
+-- set theme
+local theme = "onedark"
+
+return {
+	{
+		"folke/tokyonight.nvim",
+
+		enabled = theme == "tokyonight",
+
+		-- configs
+		config = function()
+			require("tokyonight").setup({
+				-- style: 'night', 'storm', 'day', 'moon'
+				style = "storm",
+				transparent = true,
+			})
+
+			vim.cmd.colorscheme(theme)
+		end,
+	},
+	{
+		"navarasu/onedark.nvim",
+
+		enabled = theme == "onedark",
+
+		-- configs
+		config = function()
+			require("onedark").setup({
+				-- style: 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+				style = "dark",
+				transparent = true,
+			})
+
+			vim.cmd.colorscheme(theme)
+		end,
+	},
+}
